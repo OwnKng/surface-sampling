d999f9a984c62f74d5552fe7b7116f178cac43c7
OwnKng <owen.king@outlook.com>
2021-12-19 14:02:47 +0000

    With text

--- src/Cube.tsx
+++ src/Cube.tsx
@@ -1,32 +1,52 @@
 // @ts-nocheck
-import { useFrame, useThree } from "@react-three/fiber"
-import { useLayoutEffect, useMemo, useRef, useEffect } from "react"
+import { useLayoutEffect, useMemo, useRef } from "react"
 import * as THREE from "three"
 import { MeshSurfaceSampler } from "three/examples/jsm/math/MeshSurfaceSampler"
+import { useLoader, useThree } from "@react-three/fiber"
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader"
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry"
 
 const tempObject = new THREE.Object3D()
 const tempPosition = new THREE.Vector3()
-let matrix = new THREE.Matrix4()
 
 const Cube = ({ colorArray }: any) => {
   const meshRef = useRef()
 
-  const cube = useMemo(
-    () =>
-      new THREE.Mesh(
-        new THREE.BoxGeometry(10, 10, 10),
-        new THREE.MeshBasicMaterial()
-      ),
-    []
+  const { viewport } = useThree()
+
+  const font = useLoader(FontLoader, "/bold.blob")
+
+  const config = useMemo(
+    () => ({
+      font,
+      size: 100,
+      height: 10,
+      curveSegments: 12,
+    }),
+    [font]
   )
 
-  const sampler = useMemo(() => new MeshSurfaceSampler(cube).build(), [cube])
+  const geo = useMemo(
+    () =>
+      new THREE.Mesh(
+        new TextGeometry("THREE", config),
+        new THREE.MeshBasicMaterial()
+      ),
+    [config]
+  )
+
+  const sampler = new MeshSurfaceSampler(geo).build()
 
   useLayoutEffect(() => {
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 10000; i++) {
       sampler.sample(tempPosition)
 
       tempObject.position.set(tempPosition.x, tempPosition.y, tempPosition.z)
+      tempObject.rotation.set(
+        Math.PI * Math.random(),
+        Math.PI * Math.random(),
+        Math.PI * Math.random()
+      )
       tempObject.scale.setScalar(Math.random() * 0.5 + 0.5)
       tempObject.updateMatrix()
 
@@ -35,35 +55,16 @@ const Cube = ({ colorArray }: any) => {
     meshRef.current.instanceMatrix.needsUpdate = true
   })
 
-  useFrame(({ clock }) => {
-    const time = clock.getElapsedTime()
-    for (let i = 0; i < 1000; i++) {
-      meshRef.current.getMatrixAt(i, matrix)
-
-      tempPosition.setFromMatrixPosition(matrix)
-      tempObject.position.set(tempPosition.x, tempPosition.y, tempPosition.z)
-      tempObject.rotation.set(
-        Math.sin(time / 10 + i),
-        Math.sin(time + i / 10),
-        0
-      )
-
-      tempObject.updateMatrix()
-
-      meshRef.current.setMatrixAt(i, tempObject.matrix)
-    }
-
-    meshRef.current.instanceMatrix.needsUpdate = true
-  })
-
   return (
     <instancedMesh
       receiveShadow
       castShadow
       ref={meshRef}
-      args={[null, null, 1000]}
+      args={[null, null, 10000]}
+      rotation={[-Math.PI * 0.5, 0, 0]}
+      position={[-50, 0, viewport.height]}
     >
-      <coneGeometry args={[0.25, 0.75, 3]}>
+      <coneGeometry args={[1, 3, 3]}>
         <instancedBufferAttribute
           attachObject={["attributes", "color"]}
           args={[colorArray, 3]}
@@ -76,7 +77,7 @@ const Cube = ({ colorArray }: any) => {
 
 const tempColor = new THREE.Color()
 
-const data = Array.from({ length: 1000 }, () => ({
+const data = Array.from({ length: 10000 }, () => ({
   color: ["#5ADBFF", "#006DAA", "#F15152", "#ffffff"][
     Math.floor(Math.random() * 5)
   ],
@@ -87,7 +88,7 @@ export default function CubeWrapper() {
   const colorArray = useMemo(
     () =>
       Float32Array.from(
-        new Array(1000)
+        new Array(10000)
           .fill(0)
           .flatMap((_, i) => tempColor.set(data[i].color).toArray())
       ),
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -2,14 +2,17 @@
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
 import Cube from "./Cube"
+import { Suspense } from "react"
 
 const Scene = () => {
   return (
-    <Canvas shadows>
+    <Canvas camera={{ position: [0, 20, 0] }}>
       <hemisphereLight intensity={0.4} />
-      <directionalLight intensity={0.4} position={[0, 20, 0]} />
+      <directionalLight castShadow intensity={0.4} position={[0, 20, 0]} />
       <OrbitControls />
-      <Cube />
+      <Suspense fallback={null}>
+        <Cube />
+      </Suspense>
     </Canvas>
   )
 }
