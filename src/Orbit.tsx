b86974840a19162edca603f384321f316c37c719
OwnKng <owen.king@outlook.com>
2021-12-21 15:02:55 +0000

    Lighting

--- src/Orbit.tsx
+++ src/Orbit.tsx
@@ -9,7 +9,7 @@ const Orbit = () => {
     const time = clock.getElapsedTime()
     ref.current.position.x = Math.sin(time * 3) * Math.PI * 2 * 3
     ref.current.position.z = 20 + Math.cos(time * 3) * Math.PI * 2 * 3
-    ref.current.position.y = (Math.sin(time * 3) * 0.5 + 0.5) * 30
+    ref.current.position.y = 10 + (Math.sin(time * 3) * 0.5 + 0.5) * 30
   })
 
   return <pointLight ref={ref} intensity={4.0} decay={1} distance={20} />
--- src/StageLights.tsx
+++ src/StageLights.tsx
@@ -1,9 +1,7 @@
 const StageLights = () => (
   <>
-    <spotLight position={[20, -20, 80]} angle={10} intensity={0.5} />
-    <spotLight position={[-20, -20, 80]} angle={10} intensity={0.5} />
-    <hemisphereLight intensity={0.1} />
-    <directionalLight intensity={0.1} position={[0, 80, 0]} />
+    <hemisphereLight intensity={0.2} />
+    <directionalLight intensity={0.5} position={[0, 80, 0]} />
   </>
 )
 
